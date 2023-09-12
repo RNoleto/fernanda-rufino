1316e894f833c28d16cfe6c73bc19c2c5a404757
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-09-11 21:59:58 -0300

    primeiro commit com base do projeto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <Banner/>
+</template>
+
+<script>
+import Banner from './components/Banner.vue';
+
+export default{
+  components:{
+    Banner,
+  }
+}
+</script>
+<style lang="scss" scoped>
+  body{
+    margin: 0 !important;
+  }
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Componente de banner</h1>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'Banner'
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
